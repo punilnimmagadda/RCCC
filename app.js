72b3fcafb34b64ffd03871a3531e04526f17e622
punilnimmagadda <30356661+punilnimmagadda@users.noreply.github.com>
2019-09-22 10:40:36 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    document.getElementsByClassName("nav-item")[0].click();
+});
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("nav-item");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openNewGameSettings() {
+
+}
